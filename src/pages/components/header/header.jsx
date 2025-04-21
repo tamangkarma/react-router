c75bc13cc963261a286f76b35f5b5b76c2302c9b
Karma Tamang <tamangkarma160@gmail.com>
2025-04-21 20:31:05 +0545

    updated

--- src/pages/components/header/header.jsx
+++ src/pages/components/header/header.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import Home from "../home"
+
 
 const Header = () => {
     return (
         <div className='flex bg-gray-600 justify-between items-center p-4'>
             <h1 className='text-white text-3xl font-bold'>My Website</h1>
             <nav className='flex space-x-4'>
-                
+                <a href="/" className='text-white hover:text-gray-300'>Home</a>
+                <a href="/about" className='text-white hover:text-gray-300'>About</a>
+                <a href="/blog" className='text-white hover:text-gray-300'>Blog</a>
+                <a href="/contact" className='text-white hover:text-gray-300'>Contact</a>
+                <a href="/projects" className='text-white hover:text-gray-300'>Projects</a>
             </nav>
         </div>
         
--- src/pages/components/home.jsx
+++ src/pages/components/home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
         <p className='text-center'>This is a simple example of a React component with Tailwind CSS styling.</p>
         <div className='flex justify-center mt-4'>
         <button className='bg-blue-500 text-white px-4 py-2 rounded'>Click Me</button>
+        
         </div>
     </div>
 )
